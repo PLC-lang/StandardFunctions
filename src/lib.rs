870a6461cca81e65498cdb3fb62f1c6626946d26
Ghaith Hachem <ghaith.hachem@bachmann.info>
2022-02-07 13:27:42 +0100

    Add project structure an a sample test
    Created a rust project for the functions
    Added a sample implementation and tests for the ST side

---
+++ src/lib.rs
@@ -0,0 +1,1 @@
+// Definitions of the core standard function modules for IEC61131-3
---
+++ tests/numerical_functions.rs
@@ -0,0 +1,18 @@
+use rusty::runner::{compile_and_run, MainType};
+
+// Import common functionality into the integration tests
+mod common;
+
+use common::add_std;
+
+#[test]
+fn absolute_on_int_test() {
+    let src = r"FUNCTION main : INT
+            main := ABS(-99);
+        ";
+   let sources  =  add_std!(src, "num.st");
+   let mut maintype = MainType::default();
+   let res : i16 = compile_and_run(sources, &mut maintype);
+   assert_eq!(res, 99);
+
+}
