f56f2934de99989ebba505cceb53c47977c4a5a0
Ghaith Hachem <ghaith.hachem@gmail.com>
2022-05-29 08:57:42 +0200

    Update the standard function interfaces for the new function call standard

--- src/bit_shift.rs
+++ src/bit_shift.rs
@@ -1,119 +1,113 @@
 /// Defines shift operations
 
-#[repr(C)]
-pub struct BitwiseParam<T> {
-    pub input: T,
-    pub n: u32,
-}
-
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift left operation on bytes
-pub fn SHL__BYTE(param: &BitwiseParam<u8>) -> u8 {
-    param.input << param.n
+pub fn SHL__BYTE(input: u8, n: u32) -> u8 {
+    input << n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift left operation on word
-pub fn SHL__WORD(param: &BitwiseParam<u16>) -> u16 {
-    param.input << param.n
+pub fn SHL__WORD(input: u16, n: u32) -> u16 {
+    input << n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift left operation on dword
-pub fn SHL__DWORD(param: &BitwiseParam<u32>) -> u32 {
-    param.input << param.n
+pub fn SHL__DWORD(input: u32, n: u32) -> u32 {
+    input << n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift left operation on lword
-pub fn SHL__LWORD(param: &BitwiseParam<u64>) -> u64 {
-    param.input << param.n
+pub fn SHL__LWORD(input: u64, n: u32) -> u64 {
+    input << n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift right operation on bytes
-pub fn SHR__BYTE(param: &BitwiseParam<u8>) -> u8 {
-    param.input >> param.n
+pub fn SHR__BYTE(input: u8, n: u32) -> u8 {
+    input >> n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift right operation on word
-pub fn SHR__WORD(param: &BitwiseParam<u16>) -> u16 {
-    param.input >> param.n
+pub fn SHR__WORD(input: u16, n: u32) -> u16 {
+    input >> n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift right operation on dword
-pub fn SHR__DWORD(param: &BitwiseParam<u32>) -> u32 {
-    param.input >> param.n
+pub fn SHR__DWORD(input: u32, n: u32) -> u32 {
+    input >> n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Shift right operation on lword
-pub fn SHR__LWORD(param: &BitwiseParam<u64>) -> u64 {
-    param.input >> param.n
+pub fn SHR__LWORD(input: u64, n: u32) -> u64 {
+    input >> n
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate left operation on bytes
-pub fn ROL__BYTE(param: &BitwiseParam<u8>) -> u8 {
-    param.input.rotate_left(param.n)
+pub fn ROL__BYTE(input: u8, n: u32) -> u8 {
+    input.rotate_left(n)
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate left operation on word
-pub fn ROL__WORD(param: &BitwiseParam<u16>) -> u16 {
-    param.input.rotate_left(param.n)
+pub fn ROL__WORD(input: u16, n: u32) -> u16 {
+    input.rotate_left(n)
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate left operation on dword
-pub fn ROL__DWORD(param: &BitwiseParam<u32>) -> u32 {
-    param.input.rotate_left(param.n)
+pub fn ROL__DWORD(input: u32, n: u32) -> u32 {
+    input.rotate_left(n)
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate left operation on lword
-pub fn ROL__LWORD(param: &BitwiseParam<u64>) -> u64 {
-    param.input.rotate_left(param.n)
+pub fn ROL__LWORD(input: u64, n: u32) -> u64 {
+    input.rotate_left(n)
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate right operation on bytes
-pub fn ROR__BYTE(param: &BitwiseParam<u8>) -> u8 {
-    param.input.rotate_right(param.n)
+pub fn ROR__BYTE(input: u8, n: u32) -> u8 {
+    input.rotate_right(n)
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate right operation on word
-pub fn ROR__WORD(param: &BitwiseParam<u16>) -> u16 {
-    param.input.rotate_right(param.n)
+pub fn ROR__WORD(input: u16, n: u32) -> u16 {
+    input.rotate_right(n)
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate right operation on dword
-pub fn ROR__DWORD(param: &BitwiseParam<u32>) -> u32 {
-    param.input.rotate_right(param.n)
+pub fn ROR__DWORD(input: u32, n: u32) -> u32 {
+    input.rotate_right(n)
 }
 
 #[allow(non_snake_case)]
 #[no_mangle]
 /// Rotate right operation on lword
-pub fn ROR__LWORD(param: &BitwiseParam<u64>) -> u64 {
-    param.input.rotate_right(param.n)
+pub fn ROR__LWORD(input: u64, n: u32) -> u64 {
+    input.rotate_right(n)
 }
