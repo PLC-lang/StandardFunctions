e43999a28e200adbcf21cb52bbc2a56a7fdb12c1
Nikola Milic <nikola.milic@bachmann.info>
2022-06-02 08:19:45 +0000

    #35 added concat dt/ldt and test

--- tests/time_extra_functions.rs
+++ tests/time_extra_functions.rs
@@ -186,3 +186,87 @@ fn concat_ltod_unsigned_ints() {
     assert_eq!(anytype.c, 72912341);
     assert_eq!(anytype.d, 72912341);
 }
+
+#[test]
+fn concat_dt_signed_ints() {
+    let src = "
+	PROGRAM main
+	VAR
+		a : DT;
+		b : DT;
+		c : DT;
+	END_VAR
+		a := CONCAT_DT(INT#2000,SINT#1,SINT#2,SINT#20,SINT#15,SINT#12,SINT#111);
+		b := CONCAT_DT(DINT#2000,DINT#1,DINT#2,DINT#20,DINT#15,DINT#12,DINT#111);
+		c := CONCAT_DT(LINT#2000,LINT#1,LINT#2,LINT#20,LINT#15,LINT#12,LINT#111);
+	END_PROGRAM";
+    let sources = add_std!(src, "time_extra_functions.st");
+    let mut anytype = AnyIntType::default();
+    let _: i64 = compile_and_run(sources, &mut anytype);
+    assert_eq!(anytype.a, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+}
+
+#[test]
+fn concat_dt_unsigned_ints() {
+    let src = "
+	PROGRAM main
+	VAR
+		a : DT;
+		b : DT;
+		c : DT;
+	END_VAR
+		a := CONCAT_DT(UINT#2000,USINT#1,USINT#2,USINT#20,USINT#15,USINT#12,USINT#111);
+		b := CONCAT_DT(UDINT#2000,UDINT#1,UDINT#2,UDINT#20,UDINT#15,UDINT#12,UDINT#111);
+		c := CONCAT_DT(ULINT#2000,ULINT#1,ULINT#2,ULINT#20,ULINT#15,ULINT#12,ULINT#111);
+	END_PROGRAM";
+    let sources = add_std!(src, "time_extra_functions.st");
+    let mut anytype = AnyIntType::default();
+    let _: i64 = compile_and_run(sources, &mut anytype);
+    assert_eq!(anytype.a, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+}
+
+#[test]
+fn concat_ldt_signed_ints() {
+    let src = "
+	PROGRAM main
+	VAR
+		a : LDT;
+		b : LDT;
+		c : LDT;
+	END_VAR
+		a := CONCAT_LDT(INT#2000,SINT#1,SINT#2,SINT#20,SINT#15,SINT#12,SINT#111);
+		b := CONCAT_LDT(DINT#2000,DINT#1,DINT#2,DINT#20,DINT#15,DINT#12,DINT#111);
+		c := CONCAT_LDT(LINT#2000,LINT#1,LINT#2,LINT#20,LINT#15,LINT#12,LINT#111);
+	END_PROGRAM";
+    let sources = add_std!(src, "time_extra_functions.st");
+    let mut anytype = AnyIntType::default();
+    let _: i64 = compile_and_run(sources, &mut anytype);
+    assert_eq!(anytype.a, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+}
+
+#[test]
+fn concat_ldt_unsigned_ints() {
+    let src = "
+	PROGRAM main
+	VAR
+		a : LDT;
+		b : LDT;
+		c : LDT;
+	END_VAR
+		a := CONCAT_LDT(UINT#2000,USINT#1,USINT#2,USINT#20,USINT#15,USINT#12,USINT#111);
+		b := CONCAT_LDT(UDINT#2000,UDINT#1,UDINT#2,UDINT#20,UDINT#15,UDINT#12,UDINT#111);
+		c := CONCAT_LDT(ULINT#2000,ULINT#1,ULINT#2,ULINT#20,ULINT#15,ULINT#12,ULINT#111);
+	END_PROGRAM";
+    let sources = add_std!(src, "time_extra_functions.st");
+    let mut anytype = AnyIntType::default();
+    let _: i64 = compile_and_run(sources, &mut anytype);
+    assert_eq!(anytype.a, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+    assert_eq!(anytype.b, 946844112111);
+}
