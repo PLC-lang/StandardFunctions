fb408fd4079517d1c26476f30cbf97b8d9a330d5
Michael Haselberger <michael.haselberger@bachmann.info>
2022-08-10 15:43:50 +0000

    Started with extended functions

--- src/string_functions.rs
+++ src/string_functions.rs
@@ -16,7 +16,7 @@ pub fn LEN__STRING(input: &str) -> i32 {
 ///
 #[allow(non_snake_case)]
 #[no_mangle]
-pub fn LEN__STRING(in1: &str, in2: &str) -> i32 {
+pub fn FIND__STRING(in1: &str, in2: &str) -> i32 {
     in1.find(in2).unwrap_or_default() as i32
 }
 
@@ -26,22 +26,21 @@ pub fn LEN__STRING(in1: &str, in2: &str) -> i32 {
 #[allow(non_snake_case)]
 #[no_mangle]
 pub unsafe extern "C" fn LEFT_EXT__STRING(src: *const u8, substr_len: u16, dest: *mut u8) {
-    let mut cnt = 0;
+    let mut len = 0;
     // find length of src string 
-    let len = loop {
-        if src.add(cnt).is_null() || *(src.add(cnt)) == 0 {
-            break cnt;
-        }
-        cnt += 1;
+    while !(src.add(len).is_null() || *(src.add(len)) == 0) {
+        len += 1;
     };
 
-    if len == 0 || len < substr_len {
+    if len == 0 || len < substr_len as usize {
         return;
     }  
     
-    for i in 0..len{
+    for i in 0..substr_len as usize{
         *(dest.add(i)) = *(src.add(i));
     }
+
+    *(dest.add(len)) = b'\0';
 }
 
 /// .
@@ -49,20 +48,21 @@ pub unsafe extern "C" fn LEFT_EXT__STRING(src: *const u8, substr_len: u16, dest:
 ///
 #[allow(non_snake_case)]
 #[no_mangle]
-pub unsafe extern "C" fn RIGHT_EXT__STRING(src: *const i8, substr_len: u16, dest: *mut u8) {
-    let mut cnt = 0;
-    let len = loop {
-        if src.add(cnt).is_null() || *(src.add(cnt)) == 0 {
-            break cnt;
-        }
-        cnt += 1;
+pub unsafe extern "C" fn RIGHT_EXT__STRING(src: *const u8, substr_len: u16, dest: *mut u8) {
+    let mut len = 0;
+    // find length of src string 
+    while !(src.add(len).is_null() || *(src.add(len)) == 0) {
+        len += 1;
     };
 
-    if len == 0 || len < substr_len {
+    if len == 0 || len < substr_len as usize {
         return;
     }  
     
-    for i in 0..len {
-        *(dest.add(i)) = *(src.add(i));
+    let offset = len - substr_len as usize;
+    for i in 0..substr_len as usize {
+        *(dest.add(i)) = *(src.add(i + offset));
     }
+
+    *(dest.add(len)) = 0;
 }
