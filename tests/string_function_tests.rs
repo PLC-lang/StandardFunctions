e166e2637a5a601ef16c99c23a00742f6dbee9ec
Michael Haselberger <michael.haselberger@bachmann.info>
2022-08-12 12:20:36 +0000

    Added ST generics for extended string functions. Added ST variants for string wrapper functions. Added function prototypes to mod.rs.

--- tests/string_function_tests.rs
+++ tests/string_function_tests.rs
@@ -1,6 +1,18 @@
-use common::compile_and_run;
-
 // Import common functionality into the integration tests
 mod common;
 
 use common::add_std;
+
+use crate::common::compile_and_run_no_params;
+
+#[test]
+fn len_string() {
+    let src = r#"
+	FUNCTION main : STRING
+		main := LEN(STRING#"hello");
+    END_FUNCTION
+        "#;
+    let sources = add_std!(src, "string_functions.st");
+    let res: i32 = compile_and_run_no_params(sources);
+    assert_eq!(5, res);
+}
